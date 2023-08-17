6c815f4018aa9b60117760a74493ffc4779f755c
Elias Bianchi Hernandez <elias.bianchi@sofka.com.co>
2023-08-17 20:59:12 -0300

    Plantilla de aplicación Móvil. #3
    Se generó una plantilla vacía de React Native

---
+++ DEAH-App/App.js
@@ -0,0 +1,14 @@
+import Main from './src/Main';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    // <View>
+    //   <Text style={{
+    //     paddingTop: Constants.statusBarHeight,
+    //   }}>Hola Elias</Text>
+    // </View>
+    <Main />
+  );
+}
