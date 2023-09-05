3f389db55d8a1d8c3aa31b3f082256221bdd76bd
Elias Bianchi Hernandez <elias.bianchi@sofka.com.co>
2023-09-05 20:40:49 -0300

    App Template v2.0, #3

---
+++ mobileapp-reactnative-ble/src/Main.tsx
@@ -0,0 +1,16 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const Main = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+});
+
+export default Main;
