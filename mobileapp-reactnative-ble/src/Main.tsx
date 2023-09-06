0363a9da0607550dda7d758e968c35bc692bfa09
Elias Bianchi Hernandez <elias.bianchi@sofka.com.co>
2023-09-05 21:34:37 -0300

    App Template v2.1, #3

--- mobileapp-reactnative-ble/src/Main.tsx
+++ mobileapp-reactnative-ble/src/Main.tsx
@@ -1,15 +1,78 @@
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from 'react-native';
+
+// import DeviceModal from "./components/DeviceConnectionModal";
+// import useBLE from './components/useBle';
 
 const Main = () => {
+    /* const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+    const {
+        requestPermissions,
+        scanForPeripherals,
+        allDevices
+    } = useBLE();
+
+    const scanForDevices = async () => {
+        const isPermissionsEnabled = await requestPermissions();
+        if (isPermissionsEnabled) {
+            scanForPeripherals();
+        }
+    };
+
+    const hideModal = () => {
+        setIsModalVisible(false);
+    };
+
+    const openModal = async () => {
+        scanForDevices();
+        setIsModalVisible(true);
+    }; */
+
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text>Mobile App - React Native - BLE</Text>
+            
+            {/* <TouchableOpacity onPress={openModal} style={styles.ctaButton}>
+                <Text style={styles.ctaButtonText}>Connect</Text>
+            </TouchableOpacity>
+
+            <DeviceModal
+                closeModal={hideModal}
+                visible={isModalVisible}
+                connectToPeripheral={() => { }}
+                devices={allDevices}
+            /> */}
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    ctaButton: {
+        backgroundColor: "#FF6060",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        marginHorizontal: 20,
+        marginBottom: 5,
+        borderRadius: 8,
+    },
+    ctaButtonText: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white",
     },
 });
 
