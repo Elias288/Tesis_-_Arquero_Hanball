8f7d69e8ef918bdd6223d48c788a4999d0e33103
Matías Cristaldo <matiascristal22@gmail.com>
2023-10-05 19:20:43 -0300

    Navegacion usando TabBar. #3

--- mobileapp-reactnative-ble/src/Main.tsx
+++ mobileapp-reactnative-ble/src/Main.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { BleError, Device } from 'react-native-ble-plx';
 
-import Home from './pages/Home';
-import Jugar from './pages/Jugar';
-import JugarSec from './pages/JugarSecuencia';
 import ListJugadores from './pages/List';
 import Secuencias from './pages/Secuencias';
+import AllScreens from './pages/AllScreens';
 
 type homeProps = {
   requestPermissions(): Promise<boolean>;
@@ -22,37 +20,95 @@ type homeProps = {
   espStatus: Boolean;
 };
 /************************************************* Main *************************************************/
-export type stackScreens = {
-  Home: homeProps;
-  Jugar: undefined;
-  List: undefined;
-  Secuencias: undefined;
-  JugarSec: { jug: string };
-};
-
-const Stack = createNativeStackNavigator<stackScreens>();
+type screenType={
+  Home:undefined,
+  List: {jug:string};
+  Secuencia:undefined,
+  AllScreens:undefined
+}
+const Tab = createBottomTabNavigator<screenType>();
 
 const Main = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Jugar" component={Jugar} />
-        <Stack.Screen name="List" component={ListJugadores} />
-        <Stack.Screen name="Secuencias" component={Secuencias} />
-        <Stack.Screen name="JugarSec" component={JugarSec} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName='Home'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = '';
+            switch ( route.name ) {
+                case 'Home':
+                    iconName = focused ? 'home' : 'home-outline';
+                    break;
+                case 'List':
+                    iconName = focused ? 'account-group' : 'account-group-outline';
+                    break;
+                case 'Secuencia':
+                    iconName = focused ? 'clipboard-list' : 'clipboard-list-outline';
+                    break;
+            }
+            return <Icon name = { iconName } size = {30} color="#3CB371"/>
+        }
+        })}>
+            <Tab.Screen name="Home" component={AllScreens} 
+                options={{
+                    headerStyle: {
+                        backgroundColor: "#3CB371",
+                    },
+                    headerTitleAlign: "center",
+                    headerTitle: () => (
+                        <Text style={styles.header}>DEAH App</Text>
+                    ), 
+                    headerRight: () => (
+                        <Icon name = 'cog' size={30} color="#ffffff"/>
+                    ),
+                    headerLeft: () => (
+                        <Text style={styles.header}>BLE Status</Text>
+                    ),
+                }}/>
+            <Tab.Screen name="List" component={ListJugadores} 
+                options={{
+                    headerStyle: {
+                        backgroundColor: "#3CB371",
+                    },
+                    headerTitleAlign: "center",
+                    headerTitle: () => (
+                        <Text style={styles.header}>Lista de Jugadores</Text>
+                    ), 
+                    headerRight: () => (
+                        <Icon name = 'cog' size={30} color="#ffffff"/>
+                    ),
+                    headerLeft: () => (
+                        <Text style={styles.header}>BLE Status</Text>
+                    ),
+                }}/>
+            <Tab.Screen name="Secuencia" component={Secuencias} 
+                options={{
+                    headerStyle: {
+                        backgroundColor: "#3CB371",
+                    },
+                    headerTitleAlign: "center",
+                    headerTitle: () => (
+                        <Text style={styles.header}>Secuencias</Text>
+                    ), 
+                    headerRight: () => (
+                        <Icon name = 'cog' size={30} color="#ffffff"/>
+                    ),
+                    headerLeft: () => (
+                        <Text style={styles.header}>BLE Status</Text>
+                    ),
+                }}/>
+        </Tab.Navigator>
     </NavigationContainer>
   );
 };
 
 /*********************************************** Styles ************************************************/
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight + 20,
-    justifyContent: 'center',
-  },
+  header:{
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#ffffff"
+}
 });
 
 export default Main;
---
+++ mobileapp-reactnative-ble/src/pages/Agregar_Jug.tsx
@@ -0,0 +1,79 @@
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TextComponent, View } from "react-native";
+import React, {useState} from 'react';
+import { TouchableOpacity } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { stackScreens } from './AllScreens';
+import { TextInput } from "react-native";
+import { Button } from "react-native";
+
+type propsType= NativeStackScreenProps<stackScreens,'Agregar_Jug'>;
+
+const Agregar_Jug = (props:propsType) => {
+    const {navigation} = props;
+    const gotoList_Jugadores = (nomJug:string) => {
+        navigation.navigate('List',{jug:nomJug})
+    };
+
+    const [name, setName] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.heading}>Agregar jugador</Text>
+            </View>
+            <View>
+                <TextInput style={styles.textInput} placeholder="Nombres"
+                    onChangeText={(name) => setName(name)}>
+                </TextInput>
+                <TextInput style={styles.textInput} placeholder="Apellidos"></TextInput>
+            </View>
+            <View style={styles.button}>
+                <Button
+                    title="Agregar"
+                    color="#3CB371"
+                    onPress={ () => gotoList_Jugadores(name)}/>
+            </View>
+        </View>
+    )
+}
+
+export default Agregar_Jug
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+        justifyContent:"flex-start",
+        alignItems:"center",
+      },
+    heading:{
+        fontSize:30,
+        fontWeight:"bold",
+        marginBottom: 10,
+        padding: 5
+    },
+    button:{
+        width: 200,
+        borderRadius: 10,
+    },
+    textInput:{
+        borderWidth: 1.5,
+        borderRadius: 10,
+        borderColor: '#dcdcdc',
+        width: 300,
+        padding:10,
+        marginBottom: 5,
+    },
+    item: {
+        backgroundColor: '#f5f5f5',
+        padding: 20,
+        marginVertical: 5,
+        marginHorizontal: 10,
+        width:350,
+        flexDirection: 'row'
+      },
+      title: {
+        flex:4,
+        fontSize: 18
+      }
+});
