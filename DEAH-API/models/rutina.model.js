7384afc3ed847f6a3950f86a5a72a5eefa0912ff
Elias288 <bianchi.elias@gmail.com>
2023-10-31 20:09:49 -0300

    Tipos de datos en api #17
    Co-authored-by: DieterPreuss <DieterPreuss@users.noreply.github.com>

--- DEAH-API/models/rutina.model.js
+++ DEAH-API/models/rutina.model.js
@@ -1,50 +1,35 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
 const ObjectID = mongoose.Schema.Types.ObjectId;
 
-var RutinaSchema = new Schema({
+var RutinaSchema = new Schema(
+  {
     _id: {
-        type: ObjectID,
-        auto: true
+      type: ObjectID,
+      auto: true,
     },
-    fecha: {
-        type: Date,
+    titulo: {
+      type: String,
+      require: true,
+    },
+    createDate: {
+      type: Date,
+      require: true,
+    },
+    secuencias: {
+      type: String,
+      require: true,
+    },
+    createDate: {
+      type: Date,
     },
     id_usuario: {
-        type: String,
+      type: String,
     },
-    id_jugador: {
-        type: String,
-    },
-    secuencias: { type: ObjectID, ref: 'Secuencia' },//Array<Id_secuencia>
-}, { versionKey: false });
+  },
+  { versionKey: false }
+);
 
-// Exportar el modelo
-module.exports = mongoose.model('Rutina', RutinaSchema);
-/*
-Id_rutina: String
-Fecha: Date
-Id_Usuario: String
-Id_Jugador: String
-Secuencias: Array<Id_secuencia>
-
-*******IMPORTANT*******
------------------------------------------------------------
-const { Schema, model } = require('mongoose')
-
-const matchSchema = new Schema({
-    id: String,
-    players: [
-        { type: Schema.Types.ObjectId, ref: 'Player' }
-    ],
-    winner: { type: Schema.Types.ObjectId, ref: 'Player' },
-    points: Number
-    
-}, { versionKey: false })
-
-module.exports = model('Match', matchSchema)
------------------------------------------------------------
-
-*/
+module.exports = mongoose.model("Rutina", RutinaSchema);
--- DEAH-API/models/usuario.model.js
+++ DEAH-API/models/usuario.model.js
@@ -1,64 +1,68 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
 
 var Schema = mongoose.Schema;
 
 const ObjectID = mongoose.Schema.Types.ObjectId;
 
-var UsuarioSchema = new Schema({
+var UsuarioSchema = new Schema(
+  {
     _id: {
-        type: ObjectID,
-        auto: true
+      type: ObjectID,
+      auto: true,
     },
     username: {
-        type: String,
-        required: true,
-        validate: [
-            function (name) {
-                return name.length <= 50;
-            },
-            'El nombre no deberia exceder los 50 caracteres '],
+      type: String,
+      required: true,
+      validate: [
+        function (name) {
+          return name.length <= 50;
+        },
+        "El nombre no deberia exceder los 50 caracteres ",
+      ],
     },
     contrasenia: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     nombre: {
-        type: String,
+      type: String,
+      required: true,
     },
-    rutinas: [{ type: ObjectID, ref: 'Rutina' }],
-    jugadores: [{ type: ObjectID, ref: 'Jugador' }],
-}, { versionKey: false });
+    rutinas: [{ type: ObjectID, ref: "Rutina" }],
+    jugadores: [{ type: ObjectID, ref: "Jugador" }],
+  },
+  { versionKey: false }
+);
 
-UsuarioSchema.pre('save', function (next) {
-    if (!this.isModified('contrasenia')) {
-        return next();
+UsuarioSchema.pre("save", function (next) {
+  if (!this.isModified("contrasenia")) {
+    return next();
+  }
+  bcrypt.hash(this.contrasenia, 10, (err, passwordHash) => {
+    if (err) {
+      return next(err);
     }
-    bcrypt.hash(this.contrasenia, 10, (err, passwordHash) => {
-        if (err) {
-            return next(err);
-        }
-        this.contrasenia = passwordHash;
-        next();
-    });
+    this.contrasenia = passwordHash;
+    next();
+  });
 });
 
 UsuarioSchema.methods.compare = function (password) {
-    bcrypt.compare(password, this.contrasenia, (err, isMatch) => {
-        if (err) {
-            return cb(err);
-        }
-        else {
-            if (!isMatch) {
-                return cb(null, isMatch);
-            }
-            return cb(null, this)
-        }
-    });
+  bcrypt.compare(password, this.contrasenia, (err, isMatch) => {
+    if (err) {
+      return cb(err);
+    } else {
+      if (!isMatch) {
+        return cb(null, isMatch);
+      }
+      return cb(null, this);
+    }
+  });
 };
 
 // Exportar el modelo
-module.exports = mongoose.model('Usuario', UsuarioSchema);
+module.exports = mongoose.model("Usuario", UsuarioSchema);
 /*
 Id_usuario: String
 Nombre: String
